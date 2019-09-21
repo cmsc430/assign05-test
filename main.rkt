312ffc4aad22601038f3dc84ef94c4730114dc0a
David Van Horn <dvanhorn@cs.umd.edu>
2019-09-21 16:52:29 -0400

    A real test.

--- main.rkt
+++ main.rkt
@@ -1,5 +1,17 @@
 #lang racket
-(require "../compile.rkt")
+(require "interp.rkt"
+         "asm/interp.rkt" ; means students can't add to their own asm
+         rackunit)
 
-(compile '(let ((x 1)) x))
+;; Code for submission needs to be in ".." directory
+(require "../compile.rkt"
+         "../parse.rkt"
+         "../lex.rkt")
 
+(check-equal?
+ (asm-interp (compile '(let ((x 1)) x)))
+ (interp '(let ((x 1)) x)))
+
+(check-equal?
+ (parse (lex-string "(let ((x 1)) x))"))
+ '(let ((x 1)) x))
